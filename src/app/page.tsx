714902f85c68aabc543706bcd802d2a382bea59a
EnesKaya <130606254+Enesk1999@users.noreply.github.com>
2024-12-31 20:22:57 +0300

    inital navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
-import Image from "next/image";
-
+import { ModeToggle } from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
+import { SignInButton, SignOutButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
-    <div>Hello World</div>
+    <div className="m-4">
+
+    <SignedOut>
+        <SignInButton mode="modal">
+          <Button>
+            Giriş Yap
+          </Button>
+        </SignInButton>
+    </SignedOut>
+    <SignedIn>
+        <UserButton/>
+      </SignedIn>
+      
+      <ModeToggle />
+    </div>
   );
 }
